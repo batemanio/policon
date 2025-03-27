acb16b4242758f942270f159276a3617b3f986d9
batemanio <leobateman21@gmail.com>
2025-03-27 21:10:04 +0000

    general update fix

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -54,7 +54,6 @@ export async function signup(formData: FormData) {
         password: formData.get("password") as string,
     };
 
-    console.log(varifyData(data));
     if (!varifyData(data)) {
         return;
     }
